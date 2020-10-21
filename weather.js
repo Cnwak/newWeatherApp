4bd6da141cfee903c301b2d838a686cdd5d78dfc
Chijindu <cnnwakama@gmail.com>
2020-10-21 08:53:05 +0100

    new http value

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ class Weather{
   }
 
   async getWeather(lat,lon){
-    const weatherFetch = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.key}`)
+    const weatherFetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.key}`)
 
     const weatherData = await weatherFetch.json()
 
